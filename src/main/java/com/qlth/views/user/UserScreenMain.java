3a3c4c9f2d16466e68e3844a98832609b91d8945
kimsinh20 <88784882+kimsinh20@users.noreply.github.com>
2022-09-07 17:10:40 +0700

    update

--- src/main/java/com/qlth/views/user/UserScreenMain.java
+++ src/main/java/com/qlth/views/user/UserScreenMain.java
@@ -503,7 +503,7 @@ public class UserScreenMain extends javax.swing.JFrame {
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         // TODO add your handling code here: this.dispose();
         String maGV= new GiangVien().getMaGiangVien();
-        ThongTinPhongMay ttpm = new ThongTinPhongMay("20226001");
+        ThongTinPhongMay ttpm = new ThongTinPhongMay(ShareData.nguoiDangNhap.getTenDangNhap());
         ttpm.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jMenuItem2ActionPerformed
@@ -554,7 +554,7 @@ public class UserScreenMain extends javax.swing.JFrame {
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         // TODO add your handling code here:
-       DangKiPhongMay dkPM = new DangKiPhongMay("20226001", 1, "PM02", "Tòa A4 - phòng 402");
+       DangKiPhongMay dkPM = new DangKiPhongMay(ShareData.nguoiDangNhap.getTenDangNhap(), 1, "PM02", "Tòa A4 - phòng 402");
             dkPM.setVisible(true);
 
             Toolkit toolkit = Toolkit.getDefaultToolkit();
